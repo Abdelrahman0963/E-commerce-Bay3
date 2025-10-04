468ec6ffa95424699fa51b37fb354478f1164371
Abdelrahman Sayed <110208261+Abdelrahman0963@users.noreply.github.com>
2025-10-04 04:55:54 +0300

    style: Update background and text colors in Notfound component for improved visibility

--- app/components/Notfound.tsx
+++ app/components/Notfound.tsx
@@ -4,14 +4,14 @@ import { LiaSadTear } from "react-icons/lia";
 
 const Notfound = () => {
     return (
-        <section className="min-h-screen flex flex-col items-center justify-center bg-[var(--dashboard-color)] text-white px-4 md:px-6 py-20">
+        <section className="min-h-screen flex flex-col items-center justify-center bg-white text-white px-4 md:px-6 py-20">
             <div className="text-center max-w-lg">
-                <LiaSadTear className="text-[80px] text-indigo-500 mx-auto mb-4" />
+                <LiaSadTear className="text-[80px] text-black mx-auto mb-4" />
                 <h1 className="text-5xl font-bold mb-4">404 - Page Not Found</h1>
                 <p className="text-lg text-gray-300 mb-6">
                     Sorry, the page you're looking for doesn't exist or has been moved.
                 </p>
-                <Link href="/" className="inline-block bg-indigo-600 hover:bg-indigo-700 text-white font-medium px-6 py-3 rounded transition">
+                <Link href="/" className="inline-block bg-black hover:bg-indigo-700 text-white font-medium px-6 py-3 rounded transition">
                     Go Back Home
                 </Link>
             </div>
